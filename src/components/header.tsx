ed48474f7f79c331bb3bc47acec849fe425b60b0
Guilherme <guilhermemagno09@gmail.com>
2023-04-07 01:56:42 -0300

    setting stuff on header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -29,7 +29,7 @@ function Header() {
   return (
     <header className='w-full py-5 px-8 flex items-center justify-between bg-gray-700 border-b border-gray-600'>
       <a href='/' className='text-2xl text-purple-400'>
-        Logo
+        ARGO
       </a>
 
       <button
